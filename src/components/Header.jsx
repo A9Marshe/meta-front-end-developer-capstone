3ddb8663ac9d6278b4d37c61b653adceef651ffa
ali alkhateeb <ali.alkhateb@lit-co.net>
2024-02-19 22:28:08 +0300

    implement essential header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,29 @@
+import logo from "../public/assets/logo";
+import Nav from "./Nav";
 const Header = () => {
-  return <header></header>;
+  const navEntries = [
+    "Home",
+    "About",
+    "Menu",
+    "Reservations",
+    "Orders Online",
+    "Login",
+  ];
+  return (
+    <header>
+      <img src={logo} />
+      <Nav>
+        <ul role="navigation">
+          {navEntries.map((entry) => {
+            return (
+              <li key={entry}>
+                <a href={`/${entry}`}>{entry}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </Nav>
+    </header>
+  );
 };
 export default Header;
